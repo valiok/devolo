2c287d7e474b106ef90ff5681ec7b023a134eba3
Valentine Khutornenko <valiok1402@gmail.com>
2019-11-03 21:57:08 +0200

    Initial Commit

---
+++ DevoloTests/DevoloTests.swift
@@ -0,0 +1,34 @@
+//
+//  DevoloTests.swift
+//  DevoloTests
+//
+//  Created by Valiok Khutornenko on 03.11.2019.
+//  Copyright © 2019 Valentine Khutornenko. All rights reserved.
+//
+
+import XCTest
+@testable import Devolo
+
+class DevoloTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
